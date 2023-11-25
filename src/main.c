2d10379a9eb70399c32cb91c2d3df4d976a21893
Matheus Adiel <122126818+Matheus-Adiel@users.noreply.github.com>
2023-11-25 14:29:11 -0300

    Add a new menu.
    ------------------------------- Atualizando menus -------------------------------
    OBS: Lembrando que se achar que está ruim, pode mudar a vontade ou pedir pra que eu mudar algo ou simplesmente reijeitar meu Pull Request
    ---------------------------------------------------------------------------------
    
    Adicionei um menu novo, sendo assim totalizando 3 no momento:
    1. Menu principal
    2. Menu Manage Decks
    3. Menu Manage Deck
    
    (Se os nomes estiver muito ruim e que possa confundir pode pedir pra eu mudar)
    
    No menu principal tem os seguintes elementos:
    1. Menu manage decks
    2. Back
    
    O que cada opção faz:
    Não mudou o que cada um faz
    
    No menu Manage Decks tem os seguintes elementos:
    1. Create Deck
    2. Delete Deck
    3. Update Deck
    4. Manage Deck
    5. Save Decks to File
    6. Load Decks from File
    7. Back
    
    O que cada opação faz:
    Não mudou muito o que cada um faz apenas foi adicionado a opção "4. Manage Deck" que irá levar para o gerenciamento do deck que o usuário escolher, nele será possível ver todos os flashCards que tem nesse deck e fazer todas as ações relacionadas com flashCard já dentro de um deck escolhido.
    
    No menu Manage Deck tem os seguintes elementos:
    1. Create Flashcard
    2. Delete Flashcard
    3. Update Flashcard
    4. Back
    
    O que cada opação faz:
    Assim como os anteriores, não mudou o que cada opção faz. Entretanto, agora assim que entra nesse menu, na parte superior, é listado todos os flashCards que possui nesse Deck. Ademais, quando escolhemos a ação que desejamos realizar, não é mais pedido em que Deck será realizado essa ação, ele já irá fazer as modificações nos cards que estão dentro do deck que você escolheu anteriormente.
    
    OBS: ainda falta adicionar e modificar muitas coisas para deixar os menus mais amigáveis. Se quiser que eu faça modificações ou certas adições pode falar para que eu possa fazer!

--- src/main.c
+++ src/main.c
@@ -55,44 +55,32 @@ void actionUpdateDeck(cJSON* user_context){
     updateDeck(user_context, position, newLabel);
 }
 
-void actionCreateFlashcard(cJSON* user_context){
+void actionCreateFlashcard(cJSON* user_context, int deck_position){
     char front[256];
     char back[256];
-    int deck_position;
     printf("Enter the front of the flashcard: ");
     scanf("%s", front);
     printf("Enter the back of the flashcard: ");
     scanf("%s", back);
-    printf("Enter the position of the deck: ");
-    scanf("%d", &deck_position);
     Flashcard* card = createFlashcard(front, back);
     addFlashcardToDeck(user_context, deck_position, card);
 }
 
-void actionDeleteFlashcard(cJSON* user_context){
-    int deck_position;
+void actionDeleteFlashcard(cJSON* user_context, int deck_position){
     int flashcard_position;
-    printf("Enter the position of the deck: ");
-    scanf("%d", &deck_position);
     printf("Enter the position of the flashcard: ");
     scanf("%d", &flashcard_position);
     removeFlashcardFromDeck(user_context, deck_position, flashcard_position); 
 }
 
-void actionViewFlashcards(cJSON* user_context){
-    int deck_position;
-    printf("Enter the position of the deck: ");
-    scanf("%d", &deck_position);
+void actionViewFlashcards(cJSON* user_context, int deck_position){
     viewFlashcardsFromDeck(user_context, deck_position);
 }
 
-void actionUpdateFlashcard(cJSON* user_context){
-    int deck_position;
+void actionUpdateFlashcard(cJSON* user_context, int deck_position){
     int flashcard_position;
     char newFront[256];
     char newBack[256];
-    printf("Enter the position of the deck: ");
-    scanf("%d", &deck_position);
     printf("Enter the position of the flashcard: ");
     scanf("%d", &flashcard_position);
     printf("Enter the new front of the flashcard: ");
@@ -119,22 +107,55 @@ void actionLoadDecksFromFile(cJSON* user_context){
     loadDeckFromFile(filename, user_context);
 }
 
+void actionManageDeck(cJSON* user_context){
+    int deck_position;
+    printf("Enter the position of the deck: ");
+    scanf("%d", &deck_position);
+
+    struct MenuItem manageDeckMenuItems[] = {
+        {1, "Create Flashcard", actionCreateFlashcard},
+        {2, "Delete Flashcard", actionDeleteFlashcard},
+        {3, "Update Flashcard", actionUpdateFlashcard},
+        {4, "Back", NULL},
+    };
+
+    struct Menu manageDeckMenu = {
+        "Manage Deck",
+        manageDeckMenuItems,
+        sizeof(manageDeckMenuItems) / sizeof(manageDeckMenuItems[0])
+    };
+
+    int choice;
+
+    do {
+        actionViewFlashcards(user_context, deck_position);
+        displayMenu(&manageDeckMenu);
+        choice = handleMenuInput(&manageDeckMenu);
+
+        system("clear || cls");
+        if (choice >= 1 && choice <= manageDeckMenu.numItems) {
+            if (manageDeckMenu.items[choice - 1].action != NULL) {
+                manageDeckMenu.items[choice - 1].action(user_context, deck_position);
+            }
+        } else {
+            printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != manageDeckMenu.numItems);
+}
+
 void actionManage(){
     cJSON* userdata = NULL;
     userdata = initializeUserDataBus();
     Decks* currentDecks = startDecks();
+    
     struct MenuItem manageMenuItems[] = {
         {1, "Create Deck", actionCreateDeck},
         {2, "Delete Deck", actionDeleteDeck},
-        {3, "View Decks", actionViewDecks},
-        {4, "Update Deck", actionUpdateDeck},
-        {5, "Create Flashcard", actionCreateFlashcard},
-        {6, "Delete Flashcard", actionDeleteFlashcard},
-        {7, "View Flashcards", actionViewFlashcards},
-        {8, "Update Flashcard", actionUpdateFlashcard},
-        {9, "Save Decks to File", actionSaveDecksToFile},
-        {10, "Load Decks from File", actionLoadDecksFromFile},
-        {11, "Back", NULL},
+        {3, "Update Deck", actionUpdateDeck},
+        {4, "Manage Deck", actionManageDeck},
+        {5, "Save Decks to File", actionSaveDecksToFile},
+        {6, "Load Decks from File", actionLoadDecksFromFile},
+        {7, "Back", NULL},
     };
 
     struct Menu manageMenu = {
@@ -146,23 +167,25 @@ void actionManage(){
     int choice;
 
     do {
+        actionViewDecks(userdata);
         displayMenu(&manageMenu);
         choice = handleMenuInput(&manageMenu);
 
+        system("clear || cls");
         if (choice >= 1 && choice <= manageMenu.numItems) {
             if (manageMenu.items[choice - 1].action != NULL) {
                 manageMenu.items[choice - 1].action(userdata);
-            } else {
-                printf("Goodbye!\n");
             }
         } else {
             printf("Invalid choice. Please try again.\n");
         }
     } while (choice != manageMenu.numItems);
+    system("clear || cls");
 }
 
 int main(void)
 {
+    system("clear || cls");
     cJSON* userdata = NULL;
     userdata = initializeUserDataBus();
     struct MenuItem mainMenuItems[] = {
@@ -181,7 +204,8 @@ int main(void)
     do {
         displayMenu(&mainMenu);
         choice = handleMenuInput(&mainMenu);
-
+        
+        system("clear || cls");
         if (choice >= 1 && choice <= mainMenu.numItems) {
             if (mainMenu.items[choice - 1].action != NULL) {
                 mainMenu.items[choice - 1].action(userdata);
